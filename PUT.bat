017951c6d645fdb30841c9d61c9bfe90776f8c1a
barathtm <barath@popup-mainframe.com>
2023-06-05 11:42:13 +0530

    commited

---
+++ PUT.bat
@@ -0,0 +1,25 @@
+cd C:\Barath\GitHub\CICD_TEST
+git pull 
+
+@echo off
+cd C:\Barath\GitHub\CICD_TEST\DSN\
+
+setlocal enabledelayedexpansion
+
+set CURRENTDIR="C:\Barath\GitHub\CICD_TEST"
+set FTPFILE="C:\Barath\GitHub\CICD_TEST\FTP.txt"
+
+for /F "usebackq delims=" %%G in ("dsnames") do (
+    set "folder=%%G" 	
+    
+    echo open 10.10.1.180 > "%FTPFILE%"
+    echo ibmuser >> "%FTPFILE%"
+    echo sandhata >> "%FTPFILE%"
+    echo cd /u/ibmuser/git-sync-test/ >> "%FTPFILE%" 
+    echo prompt >> "%FTPFILE%" 
+    echo PUT !folder!>> "%FTPFILE%"
+    echo quit >> "%FTPFILE%"
+    
+    ftp -s:C:\Barath\GitHub\CICD_TEST\FTP.txt
+    del C:\Barath\GitHub\CICD_TEST\FTP.txt
+)
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-(((((PDS got transfered))))) Mon 06/05/2023 10:55:29.35
+(((((PDS got transfered from ZOS to GitHub))))) Mon 06/05/2023 11:39:12.83
