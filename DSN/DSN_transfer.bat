9615d7ede541232294864caa61988971cae5c2e0
barathtm <barath@popup-mainframe.com>
2023-06-06 23:41:41 +0530

    DSN Commit

--- DSN/DSN_transfer.bat
+++ DSN/DSN_transfer.bat
@@ -1,3 +1,13 @@
-cd "C:\Barath\GitHub\CICD_TEST\DSN\"
-ftp -s:DSN_transfer.txt
-exit
+cd C:\Barath\GitHub\CICD_TEST\DSN\
+
+
+echo open 10.10.1.180 > DSN_transfer.txt
+echo ibmuser >> DSN_transfer.txt
+echo sandhata >> DSN_transfer.txt
+echo cd /u/ibmuser/git-sync-test/ >> DSN_transfer.txt 
+echo put dsnames >> DSN_transfer.txt 
+echo quit >> DSN_transfer.txt
+    
+ftp -s:C:\Barath\GitHub\CICD_TEST\DSN\DSN_transfer.txt
+del C:\Barath\GitHub\CICD_TEST\DSN\DSN_transfer.txt
+
