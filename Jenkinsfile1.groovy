24afd0478d05ea79a53963c7961adf5606f6016e
barathtm <barath@popup-mainframe.com>
2023-06-07 14:08:47 +0530

    New commits

--- Jenkinsfile1.groovy
+++ Jenkinsfile1.groovy
@@ -1,17 +1,17 @@
 pipeline {
         agent any
         stages {
-            stage('Gitsync_TEST_2') {
+            stage('Gitsync_Script2') {
                 steps {
                     script {
-                            build job: 'Gitsync_TEST_2'
+                            build job: 'Gitsync_Script2'
                         }
                     }
                }  
-            stage('Gitsync_TEST_2.1') {
+            stage('Gitsync_Script2.1') {
                 steps {
                     script {
-                            build job: 'Gitsync_TEST_2.1'
+                            build job: 'Gitsync_Script2.1'
                         }
                     }
                }    
