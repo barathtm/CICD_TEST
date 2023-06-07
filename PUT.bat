c24afd9ace8b06f51e11b616a4a5cc849321ced2
barathtm <barath@popup-mainframe.com>
2023-06-07 07:53:01 +0530

    new commit

--- PUT.bat
+++ PUT.bat
@@ -1,4 +1,8 @@
 cd C:\Barath\GitHub\CICD_TEST
+git add DSN/dsnames
+git commit -m "dsnames commit"
+git push origin main
+
 git pull 
 
 @echo off
@@ -11,13 +15,15 @@ set FTPFILE="C:\Barath\GitHub\CICD_TEST\FTP.txt"
 
 for /F "usebackq delims=" %%G in ("dsnames") do (
     set "folder=%%G" 	
-    
+
+    cd C:\Barath\GitHub\CICD_TEST\!folder!\
+
     echo open 10.10.1.180 > "%FTPFILE%"
     echo ibmuser >> "%FTPFILE%"
     echo sandhata >> "%FTPFILE%"
-    echo cd /u/ibmuser/git-sync-test/>> "%FTPFILE%" 
-    echo if not exist "!folder!" mkdir "!folder!"
-    echo cd !folder!/
+    echo cd /u/ibmuser/git-sync-test/ >> "%FTPFILE%" 
+    echo mkdir "!folder!" >> "%FTPFILE%"
+    echo cd !folder!/ >> "%FTPFILE%"
     echo prompt >> "%FTPFILE%" 
     echo mput * >> "%FTPFILE%"
     echo quit >> "%FTPFILE%"
@@ -25,3 +31,4 @@ for /F "usebackq delims=" %%G in ("dsnames") do (
     ftp -s:C:\Barath\GitHub\CICD_TEST\FTP.txt
     del C:\Barath\GitHub\CICD_TEST\FTP.txt
 )
+exit
