56041d004af70b69f8acaba48d75f5b7f9cdfed7
barathtm <110898598+barathtm@users.noreply.github.com>
2023-07-05 12:26:20 +0530

    Update Jenkinsfile1.groovy

--- Jenkinsfile1.groovy
+++ Jenkinsfile1.groovy
@@ -1,14 +1,7 @@
 pipeline {
         agent any
         stages {
-            stage('Gitsync_dsnames') {
-                steps {
-                    script {
-                            build job: 'Gitsync_dsnames'
-                        }
-                    }
-               }  
-             stage('Gitsync_Script2') {
+            stage('Gitsync_Script2') {
                 steps {
                     script {
                             build job: 'Gitsync_Script2'
